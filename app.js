aa516b479e9313171259fac789ed09094a4454a4
5419083 masudaharuki <77915680+aiki03@users.noreply.github.com>
2022-01-03 01:18:00 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+function App() {
+    return (
+      <div>
+        <header>
+          <h1>Dog Images</h1>
+        </header>
+        <body>
+            日本大学文理学部情報化学科Webプログラミングの演習課題5419083増田合氣
+        </body>
+        <body>
+            以下の画像はdog APIから取得したものである。
+            </body>
+        <main>
+          <section>
+            <figure>
+              <img
+                src="https://images.dog.ceo/breeds/affenpinscher/n02110627_1387.jpg"
+                alt="cute dog"
+              />
+            </figure>
+            <figure>
+              <img
+                src="https://images.dog.ceo/breeds/mexicanhairless/n02113978_593.jpg"
+                alt="cute dog"
+              />
+            </figure>
+          </section>
+        </main>
+        
+      </div>
+    );
+  }
+  
+  export default App;
